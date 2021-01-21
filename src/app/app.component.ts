761de633d5fcc802cf9f66fd32341b26879e2f6b
Rafael Reyes <rafaelalexanderr5@gmail.com>
2021-01-21 18:49:37 -0400

    [CHANGED] title app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Send Email confirmations';
+  title = 'Send Email confirmation';
 }
